e24e0208e8f29aeeeccdd2aef88b805148e0d702
James Boehme <jamesrbdev@gmail.com>
2021-07-06 20:55:33 -0500

    Progress toward implementation into Wraithblood.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ class Renderer {
 		this.cache      = PIXI.utils.TextureCache;
 		this.layers     = {};
 
+		PIXI.settings.SORTABLE_CHILDREN = true;
+
 		this.app = new PIXI.Application({
 			width: 256,
 			height: 256,
@@ -47,14 +49,16 @@ class Renderer {
 
 			// Other options.
 			if (options) {
-				if (options.x)        {newSprite.x        = options.x};
-				if (options.y)        {newSprite.y        = options.y};
-				if (options.width)    {newSprite.width    = options.width};
-				if (options.height)   {newSprite.height   = options.height};
-				if (options.rotation) {newSprite.rotation = options.rotation};
+				if (options.x)        {newSprite.x        = options.x;}
+				if (options.y)        {newSprite.y        = options.y;}
+				if (options.z)        {newSprite.zIndex   = options.z;}
+				if (options.width)    {newSprite.width    = options.width;}
+				if (options.height)   {newSprite.height   = options.height;}
+				if (options.rotation) {newSprite.rotation = options.rotation;}
 				if (options.layer)    {
 					if (!this.layers[options.layer]) { // Create a new layer.
 						this.layers[options.layer] = new PIXI.Container();
+						this.layers[options.layer].sortableChildren = true;
 						this.app.stage.addChild(this.layers[options.layer]);
 					}
 
@@ -105,8 +109,19 @@ class Renderer {
 }
 
 class Map extends Renderer {
-	constructor(appendTo) {
+	constructor(appendTo, mapX, mapY) {
 		super(appendTo, ["img/world/tile.png", "img/world/wall.png"]);
+
+		this.map = []
+		this.y   = mapY;
+		this.x   = mapX;
+
+		for (let y = 0; y < this.y; ++y) {
+			this.map[y] = [];
+			for (let x = 0; x < this.x; ++x) {
+				this.map[y][x] = {};
+			}
+		}
 
 		this.loader.load((loader, resources) => { // NOTE Everything in here is temporary.
 			for (let y = 0; y < 8; ++y) {
@@ -115,7 +130,7 @@ class Map extends Renderer {
 				}
 			}
 
-			this.entity("img/tokens/character.png", 0, 2, 2, 4);
+			this.entity("img/tokens/character.png", 0, 2, 2, 4, new PIXI.Polygon(34, 9, 35, 10, 35, 17, 36, 17, 37, 18, 37, 23, 38, 24, 38, 27, 39, 28, 37, 30, 36, 30, 36, 34, 35, 35, 35, 38, 36, 38, 37, 39, 37, 41, 33, 41, 33, 42, 28, 42, 28, 40, 29, 39, 29, 31, 27, 31, 26, 30, 26, 18, 29, 16, 30, 15, 29, 14, 29, 11, 31, 9));
 			this.wall(3, 0, 3, 4);
 			//this.light(128, 64, 28, 42);
 		});
@@ -130,47 +145,73 @@ class Map extends Renderer {
 		return {"x": x + startX, "y": y};
 	}
 
-	tileDown(x, y) {
+	// When a tile is mouse-down.
+	tileDown(x, y) { // (number, number)
 		
 	}
 
-	tileUp(x, y) {
+	// Mouse-up.
+	tileUp(x, y) { // (number, number)
 		
 	}
 
-	tileEnter(x, y) {
-		
+	// Hovered over.
+	tileEnter(x, y) { // (number, number)
+		this.map[y][x].sprites.tile.tint = 0x999999;
 	}
 
-	tileLeave(x, y) {
-		
+	// Unhovered.
+	tileLeave(x, y) { // (number, number)
+		this.map[y][x].sprites.tile.tint = 0xffffff;
 	}
 
-	tile(spriteX, spriteY, posX, posY) {
+	tile(spriteX, spriteY, posX, posY) { // (number, number, number, number)
 		let coords = this.gridToScreen(posX, posY);
-		this.sprite("img/world/tile.png", {x1: spriteX * 32, y1: spriteY * 32, x2: 32, y2: 32, x: coords.x, y: coords.y, layer: "tile"}, sprite => {
+		this.sprite("img/world/tile.png", {x1: spriteX * 32, y1: spriteY * 32, x2: 32, y2: 32, x: coords.x, y: coords.y, z: (posY * this.y) + posX, layer: "tile"}, sprite => {
 			// Opt-in to interactivity.
 			sprite.interactive = true;
+			sprite.hitArea     = new PIXI.Polygon(16, 0, 31, 7, 31, 23, 16, 31, 15, 31, 0, 23, 0, 7, 15, 0);
+
+			if (!this.map[posY][posX].sprites) {this.map[posY][posX].sprites = {};}
+
+			this.map[posY][posX].sprites.tile = sprite;
 
 			// Pointer events.
 			sprite.on('pointerdown', () => this.tileDown(posX, posY)).on('pointerup', () => this.tileDown(posX, posY)).on('pointerover', () => this.tileEnter(posX, posY)).on('pointerout', () => this.tileLeave(posX, posY));
-		});
-	}
 
-	wall(spriteX, spriteY, posX, posY) {
-		let coords = this.gridToScreen(posX, posY);
-		this.sprite("img/world/wall.png", {x1: spriteX * 32, y1: spriteY * 64, x2: 32, y2: 64, x: coords.x, y: coords.y - 48, z: coords.x + coords.y, layer: "tile", contain: true}, sprite => {
 			
 		});
 	}
 
-	entity(url, spriteX, spriteY, posX, posY) {
+	wall(spriteX, spriteY, posX, posY) { // (number, number, number, number)
 		let coords = this.gridToScreen(posX, posY);
-		this.sprite(url, {x1: spriteX * 64, y1: spriteY * 64, x2: 64, y2: 64, x: coords.x - 16, y: coords.y - 32, layer: "tile", contain: true}, sprite => {
+		this.sprite("img/world/wall.png", {x1: spriteX * 32, y1: spriteY * 64, x2: 32, y2: 64, x: coords.x, y: coords.y - 48, z: (posY * this.y) + posX + 2, layer: "tile", contain: true}, sprite => {
+			
+		});
+	}
+
+	entity(url, spriteX, spriteY, posX, posY, polygons) { // (string, number, number, number, number, object)
+		let coords = this.gridToScreen(posX, posY);
+		this.sprite(url, {x1: spriteX * 64, y1: spriteY * 64, x2: 64, y2: 64, x: coords.x - 16, y: coords.y - 32, z: (posY * this.y) + posX + 1, layer: "tile", contain: true}, sprite => {
 			// Opt-in to interactivity.
-			sprite.interactive = true;
+			if (polygons) {
+				sprite.hitArea     = polygons;
+				sprite.interactive = true;
+			}
+
+			sprite.on('pointerover', () => {
+				for (let i = 0; i < sprite.children.length; ++i) {
+					sprite.children[i].tint = 0xFF0000;
+				}
+			}).on('pointerout', () => {
+				for (let i = 0; i < sprite.children.length; ++i) {
+					sprite.children[i].tint = 0xffffff;
+				}
+			});
 		});
 	}
 }
 
-const game = new Map(document.body);
+const game = new Map(document.body, 10, 10);
+
+// TODO Callbacks for the wall/entity/tile functions.
